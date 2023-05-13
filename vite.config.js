389f96423ce1e7d08a8f166148533eb93efa02e5
ChefEgor <egor600@proton.me>
2023-05-13 16:57:53 +0300

    updated vite.config.js, deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/fractal/",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/fractal/'
+  }
+
+  return config
 })
